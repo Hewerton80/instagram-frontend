8a8d4956a1f0c4e4e95efad015051c3b5c388841
hewerton <hewerton80@gmail.com>
2020-01-20 14:16:30 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const url_backend = process.env.REAC_APP_URL_BACKEND || "http://localhost:3001"
+const url_backend = process.env.REACT_APP_URL_BACKEND || "http://localhost:3001"
 const api = axios.create({
     baseURL: url_backend
 })
